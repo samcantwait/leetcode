749897015f3b15a255f88aecd0dbb85095031232
Samantha Atkins <samatkins@gmail.com>
2024-12-28 08:30:32 -0600

    create directories for problem sets. Add initial problem solutions.

---
+++ leet75/mergeStringsAlternately.js
@@ -0,0 +1,17 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+var mergeAlternately = function (word1, word2) {
+  let str = "";
+  for (let i = 0; i < word1.length || i < word2.length; i++) {
+    word1[i] && (str += word1[i]);
+    word2[i] && (str += word2[i]);
+  }
+  return str;
+};
+
+const word1 = "abc";
+const word2 = "pqr";
+
+console.log(mergeAlternately(word1, word2));
